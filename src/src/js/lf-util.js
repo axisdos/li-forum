15fce2d5aa3779d1c7a704460f750092a7792f2b
thedman1361 <laverydylan@yahoo.co.uk>
2015-02-16 19:50:57 +0000

    Create util file and add say() function

---
+++ src/src/js/lf-util.js
@@ -0,0 +1,9 @@
+/*LightForum utilities script V0.0.1dev*/
+/**
+ * Output a message to the console
+ * @param  {String} c The string to be output to the console
+ * @return {Output}   The given string in the console
+ */
+function say (c) {
+	console.log(c)
+}
