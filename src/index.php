ad2cb797ef2d2e1c0b491f8e34e106005b170c2e
thedman1361 <laverydylan@yahoo.co.uk>
2015-02-16 19:40:41 +0000

    Add links required files as well as meta tags and title placeholder

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <head>
-	<title></title>
+	<title>usr-title | LightForum</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="src/thirdparty/css-reset.css">
 	<link rel="stylesheet" href="src/css/lf-style.css">
 </head>
@@ -10,5 +12,12 @@
 	<div class="lf-container">
 		
 	</div>
+
+	<!--Thirdparty Scripts-->
+	<script type="text/javascript" src="src/thirdparty/jquery-2.1.1.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.13/angular.min.js"></script>
+	<!--Scripts-->
+	<script type="text/javascript" src="src/js/lf-util.js"></script>
+	<script type="text/javascript" src="src/js/lf-script.js"></script>
 
 </body>
